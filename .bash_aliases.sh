084a226751d527c929d2295ebb17bfe072b43851
Demian Neidetcher <demian0311@gmail.com>
2015-12-31 17:02:38 -0600

    git.save.changes

--- .bash_aliases.sh
+++ .bash_aliases.sh
@@ -12,7 +12,6 @@ alias ....="cd ../../.."
 alias g='git'
 alias gd='git diff --color-words'
 alias gs='git status --short'
-#alias gl="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --"
 alias gl="git log --graph --abbrev-commit --decorate --date=relative --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all"
 
 #alias pj='ps -ef | grep java'
